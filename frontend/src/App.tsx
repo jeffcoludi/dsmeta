2c2d1fb413c98e4f5dfa67b071d8b65955f1e772
jeffcoludi <jeffcoludi@gmail.com>
2022-12-19 10:27:17 -0300

    Others Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,20 @@
-import NotificationButton from "./components/NotificationButton"
-
+import Header from "./components/Header";
+import NotificationButton from "./components/NotificationButton";
+import SalesCard from "./components/Salescard";
 function App() {
-
   return (
     <>
-   <h1>Ola</h1>
-   <NotificationButton/>
-   <NotificationButton/>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container"></div>
+          <SalesCard/>
 
-   
-   </>
-  )
-
+          
+        </section>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
