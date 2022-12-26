55e8af60930c2cb848957c743d554e9acc538492
jeffcoludi <jeffcoludi@gmail.com>
2022-12-26 11:47:45 -0300

    API Test

--- backend/src/main/java/com/dsprojeto/dsmeta/controller/SaleController.java
+++ backend/src/main/java/com/dsprojeto/dsmeta/controller/SaleController.java
@@ -1,5 +1,27 @@
 package com.dsprojeto.dsmeta.controller;
 
-public class SaleController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dsprojeto.dsmeta.entities.Sale;
+import com.dsprojeto.dsmeta.services.SaleSevice;
+
+@RestController
+@RequestMapping (value ="/sales")
+public class SaleController {
+	
+	
+	@Autowired
+	private SaleSevice service;
+	
+	@GetMapping
+	public List<Sale> findSales(){
+		return service.findSales();
+		
+		
+	}
 }
